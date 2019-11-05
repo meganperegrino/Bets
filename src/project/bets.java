695c5c91d8fa2fcb8070c319fa8936a24e1573fb
Megan Peregrino <meganperegrino@gmail.com>
2019-11-05 09:00:14 -0600

    added bets recursion code

--- src/project/bets.java
+++ src/project/bets.java
@@ -1,5 +1,22 @@
 package project;
 
 public class bets {
-
+	public static int bets(int N, int K) {
+		return betsHelper(N - 1, K, 0);
+	}
+	
+	public static int betsHelper(int N, int K, int rounds) {
+		if(N == 0) {
+			return rounds;
+		}
+		
+		if(K > 0) {
+			return betsHelper(N/2, K - 1, rounds + 1);
+		}
+		else {
+			return betsHelper(N - 1, K, rounds + 1);
+		}
+		
+	}
+	
 }
--- src/project/testJUnit.java
+++ src/project/testJUnit.java
@@ -12,4 +12,30 @@ public class testJUnit {
    public void testPrintMessage() {	  
       assertEquals(message,messageUtil.printMessage());
    }
+   
+   @Test
+   public void test1() {
+	   assertEquals(7, bets.bets(8, 0));
+   }
+   
+   @Test
+   public void test2() {
+	   assertEquals(6, bets.bets(18, 2));
+   }
+   
+   @Test
+   public void test3() {
+	   assertEquals(4, bets.bets(10, 10));
+   }
+   
+   @Test
+   public void test4() {
+	   assertEquals(0, bets.bets(1, 0));
+   }
+   
+   @Test
+   public void test5() {
+	   assertEquals(3, bets.bets(8, 3));
+   }
+   
 }
